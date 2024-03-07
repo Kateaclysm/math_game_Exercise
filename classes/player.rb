dc35400e98f7c09558678fddae181acd10a02c58
Katelynn <grayspence2@hotmail.com>
2024-03-07 17:02:53 -0500

    Added comments to clarify several aspects of code in each class.

--- classes/player.rb
+++ classes/player.rb
@@ -1,9 +1,10 @@
 class Player
+
   attr_accessor :name, :lives
 
   def initialize(name)
     @name = name
-    @lives = 3
+    @lives = 3 # => Set this to however many lives you want. 3 for brevity.
   end
 
   def answer_question(answer, current_question)
@@ -11,10 +12,12 @@ class Player
     correct_answer = current_question.num1 + current_question.num2
 
     if answer == correct_answer
-      puts "Correct! Good job, #{@name}!"
+      # => If answer is correct:
+      puts "Correct! Good job, #{@name}!" 
     else 
-      @lives -= 1
-      puts "Incorrect! Sorry #{@name}, you lose a life. CURRENT LIVES for #{@name}: #{@lives}"
+      # => If answer is INCORRECT...
+      @lives -= 1 #=> Remove a life.
+      puts "Incorrect! Sorry #{@name}, you lose a life. CURRENT LIVES for #{@name}: #{@lives}/3" # => Update current player on lives remaining.
     end
   end
 end
--- classes/question.rb
+++ classes/question.rb
@@ -1,12 +1,15 @@
 class Question
+  
   attr_reader :num1, :num2
 
   def initialize
+    # => Instance variables that will generate a number between 1 and 20 (inclusive) for each half of the equation.
     @num1 = rand(1..20)
     @num2 = rand(1..20)
   end
 
   def ask_question
+    # => Ask the question. Only addition for now.
     "what is #{@num1} + #{@num2}?"
   end
 end
